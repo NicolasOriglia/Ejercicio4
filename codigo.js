e657327b04ec6fb62127eb12e213c86c8cdf6b1a
Nicolas Origlia <noriglia@gmail.com>
2022-11-29 16:55:33 -0300

    strings guardados

---
+++ codigo.js
@@ -0,0 +1,10 @@
+let strName = 'Nicolas';
+let strLastName = 'Origlia';
+let estudiante = strName.concat(' ', strLastName);
+let estudianteMayus = estudiante.toLocaleUpperCase();
+let estudianteMinus = estudiante.toLocaleLowerCase();
+let numLetras = estudiante.length;
+let firstLetter = strName.charAt(0);
+let lastLetter = strLastName.slice(-1);
+let sinEspacios = estudiante.replace(/ /g, '');
+let encontrarName = estudiante.indexOf(strName);
